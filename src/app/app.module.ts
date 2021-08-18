45279360b1777e9b8e307e044ac447aca5b105b1
legend1910 <legend.yuva@gmail.com>
2021-08-18 23:06:22 +0530

    folder structure error fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { BasicHighlighter } from './basic-directive/basic-directive-highlighter';
-import { BetterhighlighterDirective } from './better-highlighter/betterhighlighter.directive';
+import { BetterhighlighterDirective } from './betterhighlighter.directive';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
