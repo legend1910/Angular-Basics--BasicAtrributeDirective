ff5f1ceec804aebfd4694bdff39e32d9c889a022
legend1910 <legend.yuva@gmail.com>
2021-08-18 23:54:00 +0530

    Binding properties

--- src/app/betterhighlighter.directive.ts
+++ src/app/betterhighlighter.directive.ts
@@ -1,24 +1,28 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterhighlighter]'
 })
 export class BetterhighlighterDirective implements OnInit {
+@Input() defaultColor:string='transparent';
+@Input() highlightColor:string='Yellow';
 
-  @HostBinding('style.backgroundColor') background:string='transparent';
+  @HostBinding('style.backgroundColor') background:string=this.defaultColor;
   constructor( private element:ElementRef, private render:Renderer2) { }
 ngOnInit()
 {
   // two way to change dom poperty is using render and another is using hostBinding
 // this.render.setStyle(this.element.nativeElement,'backgroundColor','purple');
-this.background='blue';
+this.background=this.highlightColor;
 }
 @HostListener('mouseenter') mouseover(eventdata:Event)
 {
-  this.render.setStyle(this.element.nativeElement,'backgroundColor','Yellow');
+ // this.render.setStyle(this.element.nativeElement,'backgroundColor','Yellow');
+ this.background=this.highlightColor;
 }
 @HostListener('mouseleave') mouseleave(eventdata:Event)
 {
-  this.render.setStyle(this.element.nativeElement,'backgroundColor','transparent');
+//  this.render.setStyle(this.element.nativeElement,'backgroundColor','transparent');
+this.background=this.defaultColor;
 }
 }
