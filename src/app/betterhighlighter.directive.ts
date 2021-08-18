db3cef16d5cea1099b4ebad4f997be00bb1f2b83
legend1910 <legend.yuva@gmail.com>
2021-08-18 23:34:28 +0530

    @HostingBinding

--- src/app/betterhighlighter.directive.ts
+++ src/app/betterhighlighter.directive.ts
@@ -1,14 +1,17 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterhighlighter]'
 })
 export class BetterhighlighterDirective implements OnInit {
 
+  @HostBinding('style.backgroundColor') background:string='transparent';
   constructor( private element:ElementRef, private render:Renderer2) { }
 ngOnInit()
 {
-  this.render.setStyle(this.element.nativeElement,'backgroundColor','purple');
+  // two way to change dom poperty is using render and another is using hostBinding
+// this.render.setStyle(this.element.nativeElement,'backgroundColor','purple');
+this.background='blue';
 }
 @HostListener('mouseenter') mouseover(eventdata:Event)
 {
